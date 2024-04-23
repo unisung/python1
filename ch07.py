7d0179483e40667c1a39b0fb504254c0f35c8934
unisung <vctor@naver.com>
2024-04-23 16:59:30 +0900

    first......
    Signed-off-by: unisung <vctor@naver.com>

---
+++ ch07.py
@@ -0,0 +1,14 @@
+#while문
+login = True
+
+while login:
+    pw = input('pw')
+    if pw=="test":
+        login =False
+        print("접속성공")
+    else:
+        print("비밀번호 오류!")
+
+# id/ pwd 입력 "admin", "test" 둘다맞으면 접속성고
+# id가 틀리면 "id오류",  "비번오류"
+# while문으로 작성
