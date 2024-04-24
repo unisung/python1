8d8a9fd25589e14c224b77c6d952dcc431ff6583
unisung <vctor@naver.com>
2024-04-24 16:19:10 +0900

    first........ second.......
    Signed-off-by: unisung <vctor@naver.com>

--- ch10.py
+++ ch10.py
@@ -14,3 +14,29 @@ c=("python",500,3.14)
 d=([1,3,2],(10,30,20),"java","C++")
 print(type(f2),type(f22),type(f),type(f1),type(a),type(b),type(c), type(d))
 
+#튜플과 리스트 변환
+data1=[1,2,3,4,5]
+data2=(1,2,3,4,5)
+v1 = tuple(data1)
+v2 = list(data2)
+print(v1,v2)
+
+data2 = list(data2)
+print(data2)
+data2[2]=6
+print(data2)
+data2=tuple(data2)
+print(data2)
+
+a=("python","C++","JAVA","GO")
+b=(4,2,5,1,3)
+print(a[1:2]) # 1<= <2
+print(a[1:3]) # 1<= <3
+print(a[-3:-1]) # -3<= <-1
+print(a.index("JAVA"))
+print(a.count("C++"))
+print(len(a))
+c=sorted(b)#원본 데이타는 변함없음, 리턴된 데이타는 정렬된list
+print(b)
+print(c)
+
