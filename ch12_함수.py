b099fbb7a25ab1d37ef03e16b14322a64417abd4
unisung <vctor@naver.com>
2024-04-25 11:49:52 +0900

    first........ second......... third...
    Signed-off-by: unisung <vctor@naver.com>

--- ch12_함수.py
+++ ch12_함수.py
@@ -22,3 +22,49 @@ fun2('일지매')
 result2=fun3()
 print(result2)
 fun4()
+
+#내장 함수
+engt="python programming"
+kot="파이썬 프로그래밍"
+pro=["OA","Prog","Multi","Prog"]
+print("len:",len(engt),len(kot), len(pro))
+print("count:",engt.count("p"), kot.count("파"), pro.count("Prog"))
+print("index:",engt.index('h'), pro.index('OA'))
+#print("index:",engt.find('h'), pro.find('OA')) #list에는 find가 없음
+print(dir([]))
+#
+print("index:",engt.find('k')) # 해당객체없으면 -1리턴
+#print("index:",engt.index('k')) # 해당객체없으면 Value Error리턴
+
+#문자열
+prog="java c++ Python html5"
+print(prog.upper())
+print(prog.lower())
+print(prog.istitle())
+res=prog.replace("c++","GO")
+print(res)
+print(prog)
+
+#문자열 연결
+text="123abc가나다"
+prog=["java","c++","python","html5"]
+print("-".join(text))
+print(", ".join(prog)) #"java, c++, python, html5"
+
+#문자분리
+t1='java py html'
+t2='java/py/html'
+print(t1.split()) #리스트형태로 출력
+print(t2.split('/'))
+
+#공백제거
+t="  korea   123  "
+print(len(t),t)
+print(len(t.lstrip()), t.lstrip())
+print(len(t.rstrip()), t.rstrip())
+print(len(t.strip()), t.strip())
+
+# ord(문자), chr(숫자)
+for a in range(65,91):
+    print(chr(a),end='')
+
