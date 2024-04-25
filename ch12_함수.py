9687c7b50493a0d0bbc66292f139bed0cde1ce80
unisung <vctor@naver.com>
2024-04-25 12:48:23 +0900

    first........ second......... third...
    Signed-off-by: unisung <vctor@naver.com>

--- ch12_함수.py
+++ ch12_함수.py
@@ -68,3 +68,105 @@ print(len(t.strip()), t.strip())
 for a in range(65,91):
     print(chr(a),end='')
 
+#숫자 함수
+print(abs(-3))
+print(pow(3,4), 3**4)
+print(max(5,2,4,8,3))
+print(min(5,2,4,8,3))
+print(round(3.147))
+print(round(3.547))
+print(round(3.147, 2)) # 반올림한 결과의 소수점자리
+
+#진법 - bin(), oct(), hex()로 변환한 결과는 str
+x=35
+b, o, h=bin(x), oct(x), hex(x) # 2진, 8진, 16
+print(b,o,h)
+print(type(b), type(o),type(h), sep='\n')
+
+# int('진법형태값',진법수) => 10진수
+print(int('0b11',2), int('11',2))
+print(int('0o11',8), int('11',8))
+print(int('0x11',16), int('11',16))
+
+# math 라이브러리
+import math
+print(math.pi)
+print(math.e)
+print(math.trunc(1.78))
+print(math.factorial(5)) # 5x4x3x2x1
+print(math.degrees(math.pi))
+print(math.radians(180))
+print(math.sin(math.pi/2))
+print(math.cos(math.pi/2))
+print(math.log10(100))
+print(math.log2(8))
+print(math.pow(3,4))
+
+#random
+import random as r
+print(r.random())
+print(r.randint(10,20)) # 10<=범위<=20
+print(r.randrange(10,20)) # 10<=범위<20
+
+#날짜/시간 함수
+import time
+print(time.time()) # 1970년 1월1일 0시0분 지난 초단위
+print(time.localtime(time.time()))
+
+import time
+t=time.localtime(time.time())
+print(f'{t.tm_year}-{t.tm_mon}-{t.tm_mday}')
+print(time.strftime('%Y-%m-%d',t))
+
+import datetime
+now = datetime.datetime.today()
+print(datetime.datetime.today())
+print(datetime.datetime.now())
+print(now.year, now.month, now.day)
+print(now.hour, now.minute, now.second)
+print(now.strftime('%Y-%M-%d (%a)'))
+print(now.strftime('%H-%M-%S'))
+
+#반환값이 여러개인 경우
+def calc(a,b):
+    return a+b, a-b, a*b
+res1,res2,res3=calc(2,3)
+print(res1,res2,res3)
+res4=calc(2,3)
+print(res4)
+
+#가변 인수를 받는 함수
+def data_sum(*n):
+    s=0
+    print(type(n))
+    for i in n:
+        s+=i
+    return s
+
+print(data_sum(1))
+print(data_sum(1,2))
+print(data_sum(1),2,3,4,5,6,7,8,9,10)
+
+# 키워드 매개변수, kwargs
+def print_kwargs(**kwargs):
+    print(type(kwargs))
+    print(kwargs)
+
+print_kwargs(a=1) # {'a':1}
+print_kwargs(name='foo', age=3) # {'name':'foo','age':3}
+
+# return - 금칙어
+def say_nick(nick):
+    if nick=='바보':
+        return
+    print("나의 별명은 %s 입니다"%nick)
+
+say_nick('아이돌')
+say_nick('바보')
+
+
+
+
+
+
+
