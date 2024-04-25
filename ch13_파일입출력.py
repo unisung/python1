824e15decb2ed7514df35059d1d461a418886a5a
unisung <vctor@naver.com>
2024-04-25 16:41:00 +0900

    first........ second......... third......
    Signed-off-by: unisung <vctor@naver.com>

--- ch13_파일입출력.py
+++ ch13_파일입출력.py
@@ -30,54 +30,57 @@ with open('c:/Temp/pro.txt', 'r', encoding='utf8') as f:
 #         if not t: break
 #         print(t, end='')
 
- # readlines()
-with open('c:/Temp/pro.txt', 'r', encoding='utf8') as f:
-    t=f.readlines()
-    for k in t:
-        print(k.replace('\n',''))
-
-
-# 기록하기
-f=open('c:\\Temp\\test.txt','w')
-f.write('python\n')
-f.write('java\n')
-f.close()
-print('저장완료')
-
-# 표준출력 print('값',파일변수)
-f=open('c:\\Temp\\test1.txt','w')
-print('python',file=f)
-print('java',file=f)
-f.close()
-print('저장완료')
-
-# 표준출력 write메소드 혼용
-with open('c:\\Temp\\test2.txt','w') as f:
-    f.write('python\n')
-    print('java',file=f)
-
-## 파이썬, 자바, 스프링, html, Django 를 한줄씩 파일에 저장 후
-## 내용을 읽어서 한줄씩 출력
-#파일명 =c:\\myfold\\test4.txt
-
-with open('c:\\Temp\\test4.txt','w') as f:
-    f.write('파이썬\n')
-    f.write('자바\n')
-    f.write('스프링\n')
-    f.write('html\n')
-    f.write('Django\n')
-    print('저장완료')
-
-with open('c:\\Temp\\test4.txt','r') as f:
-    while True:
-        t=f.readline()
-        if not t: break
-        print(t, end='')
+#  # readlines()
+# with open('c:/Temp/pro.txt', 'r', encoding='utf8') as f:
+#     t=f.readlines()
+#     for k in t:
+#         print(k.replace('\n',''))
+#
+#
+# # 기록하기
+# f=open('c:\\Temp\\test.txt','w')
+# f.write('python\n')
+# f.write('java\n')
+# f.close()
+# print('저장완료')
+#
+# # 표준출력 print('값',파일변수)
+# f=open('c:\\Temp\\test1.txt','w')
+# print('python',file=f)
+# print('java',file=f)
+# f.close()
+# print('저장완료')
+#
+# # 표준출력 write메소드 혼용
+# with open('c:\\Temp\\test2.txt','w') as f:
+#     f.write('python\n')
+#     print('java',file=f)
+#
+# ## 파이썬, 자바, 스프링, html, Django 를 한줄씩 파일에 저장 후
+# ## 내용을 읽어서 한줄씩 출력
+# #파일명 =c:\\myfold\\test4.txt
+#
+# with open('c:\\Temp\\test4.txt','w') as f:
+#     f.write('파이썬\n')
+#     f.write('자바\n')
+#     f.write('스프링\n')
+#     f.write('html\n')
+#     f.write('Django\n')
+#     print('저장완료')
+#
+# with open('c:\\Temp\\test4.txt','r') as f:
+#     while True:
+#         t=f.readline()
+#         if not t: break
+#         print(t, end='')
 
 #문자열,숫자값 입/출력 -> 객체 입출력
 import pickle
 f=open("c:\\Temp\\test5.txt",'wb') #w-write,b-binary
-data={1:'python',2:'you need'}
+blist=['A','B','AB','O','AB','O','B','AB']
+data= {}
+for x in blist:
+    data[x]=blist.count(x)
 pickle.dump(data,f)
 f.close()
 print('저장완료')
