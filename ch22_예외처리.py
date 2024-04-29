38ab3530c998a67e8aae2badd49d94bfe7e1fd2d
unisung <vctor@naver.com>
2024-04-29 16:29:13 +0900

    first........ second.......... third...........
    Signed-off-by: unisung <vctor@naver.com>

--- ch22_예외처리.py
+++ ch22_예외처리.py
@@ -100,4 +100,27 @@ eagle = Eagle()
 eagle.fly()
 
 # 사용자 정의 예외 만들기
+class MyError(Exception):
+    pass
 
+def say_nick(nick):
+    if nick=='바보':
+        raise MyError()
+    print(nick)
+
+try:
+    say_nick('천사')
+    say_nick('바보')
+except MyError as e:
+    #print('허용되지않는 별명입니다.')
+    print(e)
+
+class MyError(Exception):
+    def __str__(self):
+        return "허용되지 않는 별명입니다."
+
+try:
+    say_nick('천사')
+    say_nick('바보')
+except MyError as e:
+    print(e)
