fa2f2e258ccdeb3f2a56d93096c144075af42c1c
unisung <vctor@naver.com>
2024-05-02 11:52:55 +0900

    first........ second.......... third...........
    Signed-off-by: unisung <vctor@naver.com>

--- ch31_정규표현식.py
+++ ch31_정규표현식.py
@@ -45,3 +45,98 @@ print(pat.sub("\\g<1>-*******",data))
 # ca{2,5}t => ct(x), cat(x), caat(o), caaat(o), caaaaaat(x)
 
 # ? 갯수 {0,1} 없거나 1개인 경우 => ab?c => ac(o), abc(o), abbbc(x),
+
+##정규표현식 이용한 문자열 검색
+import re
+#match() - 문자열 처음부터 정규식과 매치되는지 조사
+# - 패턴에 맞는 값이 없으면 None
+p = re.compile('[a-z]+') # a aaa  xx x
+data ="python"
+m = p.match(data)
+print(m)
+m=p.match("3 python")
+print(m)
+##조사결과 None처리
+# p=re.compile('[a-z]+')
+# inputdata=input('조사할문자열:')
+# m=p.match(inputdata)
+# if m: #m이 None이 아니면 True
+#     print("Match found:", m.group())
+# else:
+#     print('No match')
+
+#search() - 문자열 전체를 검색하여 정규식과 매치되는지 조사 - None
+m = p.search("python")
+print(m)
+print('-------------')
+m=p.search("3 python")
+print(m)
+#findall() - 정규식과 매치되는 모든 문자열(substring)을 리스트로 반환
+result = p.findall("life is too short")
+print(result)
+#finditer() - 정규식과 매치되는 모든 문자열(substring)을 iterable객체로 반환
+result = p.finditer("life is too short")
+print(list(result))
+
+##match 메소드
+#group() - 매치된 문자열 리턴
+#start() - 매치된 문자열 시작 위치
+#end() - 매치된 문자열 끝 위치
+#span() - 매치된 문자열의 (시작,끝) 위치 튜플로 리턴
+m=p.match("python")
+print(m.group(), m.start(), m.end(), m.span())
+
+m = p.search("3 python")
+print(m.group())
+
+## re.compile() 옵션
+# DOTALL(S) - .(dot)이 줄바꿈 문자을 포함해 모든 문자오 매치될 수있게 함.
+# IGNORECASE(I) - 대소문자 구분 없이 매치
+# MULTILINE(M) - 여러줄과 매칭 되게, ^, $
+# VERBOSE(X) - 보기편하게 만듬.
+import re
+p=re.compile('a.b')
+m=p.match('a\nb') #줄바꿈문자 무시
+print(m)
+
+p=re.compile('a.b', re.S) #re.DOTALL
+m=p.match('a\nb') # 줄바꿈문자 매칭
+print(m)
+
+#IGNORECASE, I
+p= re.compile('[a-z]+',re.I)
+print(p.match('python'))
+print(p.match('python'.upper()))
+print(p.match('python'.capitalize()))
+
+# MULILINE, M
+import re
+p=re.compile("^python\s\w+", re.MULTILINE) # ^시작
+
+data="""python one
+life is too short
+python two
+you need python
+python three"""
+
+print("----------")
+print(p.findall(data))
+
+#VERBOSE, X
+charref=re.compile(r'&[#](0[0-7]+|[0-9]+|x[0-9a-fA-F]+);')
+
+charref = re.compile(r"""
+ &[#]                # Start of a numeric entity reference
+ (
+     0[0-7]+         # Octal form
+   | [0-9]+          # Decimal form
+   | x[0-9a-fA-F]+   # Hexadecimal form
+ )
+ ;                   # Trailing semicolon
+""", re.VERBOSE)
+
+#
+
+
+
+
